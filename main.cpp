2fdb07c7ac46269ff802c51b94bfc344b8c775ea
LiangTsao <LiangTsao@qq.com>
2025-04-29 18:02:23 +0800

    sss

--- main.cpp
+++ main.cpp
@@ -1,46 +1,77 @@
+#include <boost/asio.hpp>
+#include <memory>
 #include <iostream>
-#include <asio.hpp>
-#include <asio/ip/tcp.hpp>
 
-using asio::ip::tcp;
+using boost::asio::ip::tcp;
 
-// 处理客户端连接
+class Session : public std::enable_shared_from_this<Session> {
+public:
+    Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
-void handle_connection(tcp::socket socket) {
-    try {
-        std::array<char, 128> buffer;
-        asio::error_code error;
-
-        // 读取客户端数据
-        size_t length = socket.read_some(asio::buffer(buffer), error);
-        if (!error) {
-            // 将数据写回客户端
-            asio::write(socket, asio::buffer(buffer, length), error);
-        }
+    void start() {
+        do_read();
     }
-    catch (std::exception& e) {
-        std::cerr << "Exception in thread: " << e.what() << std::endl;
+
+private:
+    void do_read() {
+        auto self(shared_from_this());
+        socket_.async_read_some(
+            boost::asio::buffer(data_, max_length),
+            [this, self](std::error_code ec, std::size_t length) {
+                if (!ec) {
+                    std::cout << "Received: " << std::string(data_, length) << std::endl;
+                    do_write(length);
+                }
+            });
     }
-}
+
+    void do_write(std::size_t length) {
+        auto self(shared_from_this());
+        boost::asio::async_write(
+            socket_,
+            boost::asio::buffer(data_, length),
+            [this, self](std::error_code ec, std::size_t /*length*/) {
+                if (!ec) {
+                    do_read();
+                }
+            });
+    }
+
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+};
+
+class Server {
+public:
+    Server(boost::asio::io_context& io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+        do_accept();
+    }
+
+private:
+    void do_accept() {
+        acceptor_.async_accept(
+            [this](std::error_code ec, tcp::socket socket) {
+                if (!ec) {
+                    std::make_shared<Session>(std::move(socket))->start();
+                }
+                do_accept();
+            });
+    }
+
+    tcp::acceptor acceptor_;
+};
 
 int main() {
     try {
-        std::cerr << "Exception in aaaaaaaaaaaaaaaaaaaaathread: " ;
-        asio::io_context io_context;
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 12345));
-
-        for (;;) {
-            // 接受客户端连接
-            tcp::socket socket(io_context);
-            acceptor.accept(socket);
-            // 处理连接
-            handle_connection(std::move(socket));
-        }
+        boost::asio::io_context io_context;
+        Server server(io_context, 8080);
+        std::cout << "TCP Echo Server running on port 8080..." << std::endl;
+        io_context.run();
     }
     catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
-
     return 0;
 }
-    
